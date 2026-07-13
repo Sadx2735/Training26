31c88f4710891560275acf49c34aae88a570a8eb
sadhasivam <Sadhasivam.Saravanan@trumpf.com>
2026-07-13 12:37:07 +0530

    Third Commit - New Branch

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -2,6 +2,8 @@
 
 class Program {
     static void Main (string[] args) {
+        Console.WriteLine ("Hi to Check Diff");
+        Console.WriteLine ("Hello, World!");
         Console.WriteLine ("Hello, World!");
     }
 }
