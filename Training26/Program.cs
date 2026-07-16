ccef3b46cab496e4699fad20d16a816fea386320
sadhasivam <Sadhasivam.Saravanan@trumpf.com>
2026-07-16 11:34:49 +0530

    Correction

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -1,34 +1,9 @@
-﻿int open1 = 0;
-int open2 = 0;
+﻿namespace Training26;
 
-Console.Write ("Enter the String : ");
-string brackets = Console.ReadLine ().Trim ();
-char[] chrbrackets = brackets.ToCharArray ();
-
-bool isbalanced () {
-    foreach (var b in chrbrackets) {
-        if (b == '[' || b == ']') {
-            if (b == ']') {
-                open1 -= 1;
-                if (open1 < 0) { return false; }
-            } else { open1 += 1; }
-        } else if (b == '(' || b == ')') {
-            if (b == ')') {
-                open2 -= 1;
-                if (open2 < 0) { return false; }
-            } else { open2 += 1; }
-        } else {
-            Console.WriteLine ("Invalid");
-            return false;
-        }
+class Program {
+    static void Main (string[] args) {
+        Console.WriteLine ("Hi to Check Diff");
+        Console.WriteLine ("Hello, World!");
+        Console.WriteLine ("Hello, World!");
     }
-    if (open1 == 0 && open2 == 0) { return true; }
-    return false;
-}
-
-bool isbal = isbalanced ();
-if (isbal) {
-    Console.WriteLine ("Its Balanced..");
-} else {
-    Console.WriteLine ("Its Not Balanced");
 }
