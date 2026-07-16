f559f23a7bc13333f785b35ac5ce1cb4704b9dea
sadhasivam <Sadhasivam.Saravanan@trumpf.com>
2026-07-16 11:24:05 +0530

    Commit for Q1

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -1,9 +1,78 @@
-﻿namespace Training26;
+﻿Console.Write ("Enter the Number of Rows : ");
+int rows = Convert.ToInt32 (Console.ReadLine ().Trim ());
+Console.Write ("Enter the Number of Cols : ");
+int cols = Convert.ToInt32 (Console.ReadLine ().Trim ());
+Console.WriteLine ($"Rows {rows} and Cols of {cols}");
 
-class Program {
-    static void Main (string[] args) {
-        Console.WriteLine ("Hi to Check Diff");
-        Console.WriteLine ("Hello, World!");
-        Console.WriteLine ("Hello, World!");
+int[,] Grid = new int[rows, cols];
+for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+        Console.Write ($"Enter the Value for row {i} col {j} : ");
+        int number = Convert.ToInt32 (Console.ReadLine ().Trim ());
+        Grid[i, j] = number;
     }
+}
+
+int Diagonal1 () {
+    int total = 0;
+    int rnumber = 0;
+    int cnumber = 0;
+    while (rnumber < rows && cnumber < cols) {
+        total += Grid[rnumber, cnumber];
+        rnumber++;
+        cnumber++;
+    }
+    return total;
+}
+
+int Diagonal2 () {
+    int total = 0;
+    int rnumber = 0;
+    int cnumber = cols - 1;
+    while (rnumber < rows && cnumber >= 0) {
+        total += Grid[rnumber, cnumber];
+        rnumber++;
+        cnumber--;
+    }
+    return total;
+}
+
+int Colsum (int Colnumber) {
+    int total = 0;
+    for (int i = 0; i < rows; i++) {
+        total += Grid[i, Colnumber];
+    }
+    return total;
+}
+
+int Rowsum (int rownumber) {
+    int total = 0;
+    for (int i = 0; i < cols; i++) {
+        total += Grid[rownumber, i];
+    }
+    return total;
+}
+
+bool isMagicMatrix () {
+    int summation = Rowsum (0);
+    for (int i = 1; i < rows; i++) {
+        if (summation != Rowsum (i)) {
+            return false;
+        }
+    }
+    for (int i = 0; i < cols; i++) {
+        if (summation != Colsum (i)) {
+            return false;
+        }
+    }
+    if (Diagonal1 () != summation) { return false; }
+    if (Diagonal2 () != summation) { return false; }
+    return true;
+}
+
+bool result = isMagicMatrix ();
+if (result) {
+    Console.WriteLine ($"The Given Matrix is a Magic Matrix");
+} else {
+    Console.WriteLine ($"The Given Matrix is not Magic Matrix");
 }
